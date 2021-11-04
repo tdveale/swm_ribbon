93e0c20d8727c05aea329bf68b8a27fb2bf98ba7
Tom Veale <tom.veale@ucl.ac.uk>
2021-11-04 15:54:32 +0000

    minor changes to complete adaption from previous script. Command to extract the subject dir

--- scripts/create_swm_ribbon.sh
+++ scripts/create_swm_ribbon.sh
@@ -12,10 +12,14 @@ hemis=(lh rh)
 # extract subject ID from the freesurfer directory (needed for last command)
 fsSubjID=`basename ${freesurferDir}`
 
+# extract the dirname of the freesurfer directory (needed for last command)
+fsSubjDir=`dirname ${freesurferDir}`
+
+# loop through each hemisphere of the brain and expand the GM/WM surface (named white surface in FreeSurfer) to 2mm below (where we expect SWM to stop)
 for ihemi in ${hemis[@]}; do
   mris_expand ${freesurferDir}/surf/${ihemi}.white -2 ${freesurferDir}/surf/${ihemi}.swm-2mm-surf
 done
 
-# fill in voxels between the GM/WM boundary to -2mm below the GM/WM boundary (what we created above) - this gives us the SWM ribbon
-# the pial surface is now the white surface, the white surface is now the -2mm SWM boundary (this is the main repurposing)
-mris_volmask --surf_pial white --surf_white swm-2mm-surf --out_root swm-ribbon --save_ribbon --save_distance --sd ${isess}/anat/ ${fsSubjID}
+# fill in voxels between the GM/WM surface to the surface -2mm below (what we created above) - this gives us the SWM ribbon
+# in this command, the pial surface is now the white surface, the white surface is now the -2mm SWM boundary (this is the main repurposing)
+mris_volmask --surf_pial white --surf_white swm-2mm-surf --out_root swm-ribbon --save_ribbon --save_distance --sd ${fsSubjDir} ${fsSubjID}
